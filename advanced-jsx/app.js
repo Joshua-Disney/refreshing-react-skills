59c33aee695a62008f790daefd30bbd97aca9363
unknown <joshua.a.disney@gmail.com>
2023-01-06 21:20:30 -0800

    pausing lesson to game with Midge

--- advanced-jsx/app.js
+++ advanced-jsx/app.js
@@ -5,3 +5,21 @@ import ReactDOM from 'react-dom';
 const myDiv = <div className="big">I AM A BIG DIV</div>
 
 ReactDOM.render(myDiv, document.getElementById('app'))
+
+const profile = (
+    <div>
+      <h1>I AM JENKINS</h1>
+      <img src="images/jenkins.png" />
+      <article>
+        I LIKE TO SIT
+        <br/>
+        JENKINS IS MY NAME
+        <br/>
+        THANKS HA LOT
+      </article>
+    </div>
+  );
+
+ReactDOM.render(<h1>2 + 3</h1>, document.getElementById('app'))
+
+ReactDOM.render(<h1>{2 + 3}</h1>, document.getElementById('app'))
