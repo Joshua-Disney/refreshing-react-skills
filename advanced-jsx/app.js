69f1ced0a136ccd46025fe8a29c486554bc43327
unknown <joshua.a.disney@gmail.com>
2023-01-07 19:31:32 -0800

    Further into advanced jsx lesson

--- advanced-jsx/app.js
+++ advanced-jsx/app.js
@@ -23,3 +23,47 @@ const profile = (
 ReactDOM.render(<h1>2 + 3</h1>, document.getElementById('app'))
 
 ReactDOM.render(<h1>{2 + 3}</h1>, document.getElementById('app'))
+
+const math = <h1>2 + 3 = {2 + 3}</h1>
+const pi = (
+  <div>
+    <h1>
+      PI, YALL!
+    </h1>
+    <p>
+      {Math.PI.toFixed(20)}
+    </p>
+  </div>
+);
+
+ReactDOM.render(pi, document.getElementById('app'))
+
+const theBestString = 'tralalalala i am da best';
+
+ReactDOM.render(<h1>{theBestString}</h1>, document.getElementById('app'));
+
+const goose = 'https://content.codecademy.com/courses/React/react_photo-goose.jpg';
+
+// Declare new variable here:
+const gooseImg = (
+  <img
+    src={goose} />
+)
+
+ReactDOM.render(gooseImg, document.getElementById('app'))
+
+function makeDoggy(e) {
+  // Call this extremely useful function on an <img>.
+  // The <img> will become a picture of a doggy.
+  e.target.setAttribute('src', 'https://content.codecademy.com/courses/React/react_photo-puppy.jpeg');
+  e.target.setAttribute('alt', 'doggy');
+}
+
+const kitty = (
+	<img 
+		src="https://content.codecademy.com/courses/React/react_photo-kitty.jpg" 
+		alt="kitty"
+    onClick={makeDoggy} />
+);
+
+ReactDOM.render(kitty, document.getElementById('app'));
