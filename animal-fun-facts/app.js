7b65cc01d4cf3127d79ccad41c3ac76775c05909
unknown <joshua.a.disney@gmail.com>
2023-01-08 18:37:14 -0800

    render background and animals on screen

--- animal-fun-facts/app.js
+++ animal-fun-facts/app.js
@@ -1,3 +1,39 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { animals } from './animals';
+
+const title = ''
+const background = (
+  <img 
+    className='background' 
+    alt='ocean' 
+    src='/images/ocean.jpg'
+  />
+)
+const images = []
+for (const animal in animals) {
+  images.push(
+    <img
+      key={animal}
+      className='animal'
+      alt={animal}
+      src={animals[animal].image}
+      aria-label={animal}
+      role='button' 
+    />
+  )
+}
+
+const animalFacts = (
+  <div>
+    {background}
+    <div className='animals'>
+      {images}
+    </div>
+    <h1>
+      {title === '' ? 'Click an animal for a fun fact' : title}
+    </h1>
+  </div>
+)
+
+ReactDOM.render(animalFacts, document.getElementById('root'))
