f9e71e43117a92d6259cb9bce507cda70db820b7
unknown <joshua.a.disney@gmail.com>
2023-01-20 20:33:08 -0700

    Complete this.state lesson

--- Components-Interacting/this-state/App.js
+++ Components-Interacting/this-state/App.js
@@ -3,13 +3,16 @@ import ReactDOM from 'react-dom';
 
 class App extends React.Component {
 	// constructor method begins here:
-
+  constructor(props) {
+    super(props)
+    this.state = { title: 'Best App'}
+  }
 	
   render() {
     return (
-      <h1>
-        Wow this entire app is just an h1.
-      </h1>
+      <h1>{this.state.title}</h1>
     );
   }
 }
+
+ReactDOM.render(<App />, document.getElementById('app'))
