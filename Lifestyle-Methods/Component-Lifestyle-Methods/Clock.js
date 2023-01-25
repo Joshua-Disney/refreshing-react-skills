5fc6170454c35c9d7e030f03a751d5a719d8d855
unknown <joshua.a.disney@gmail.com>
2023-01-24 23:47:53 -0700

    Copy initial code from Codecademy

---
+++ Lifestyle-Methods/Component-Lifestyle-Methods/Clock.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class Clock extends React.Component {
+  // Add your methods in here.
+}
+
+ReactDOM.render(<Clock />, document.getElementById('app'));
