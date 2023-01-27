778a018bf09d30b25ed48a3b956c8521f103f227
unknown <joshua.a.disney@gmail.com>
2023-01-26 22:59:12 -0700

    clock.js completion pre-refactor

--- Lifestyle-Methods/Component-Lifestyle-Methods/Clock.js
+++ Lifestyle-Methods/Component-Lifestyle-Methods/Clock.js
@@ -7,7 +7,13 @@ export class Clock extends React.Component {
     this.state = { date: new Date() };
   }
   render() {
-    return <div>{this.state.date.toLocaleTimeString()}</div>;
+    return (
+      <div>
+        {this.props.isPrecise
+          ? this.state.date.toISOString()
+          : this.state.date.toLocaleTimeString()}
+      </div>
+    );
   }
   componentDidMount() {
     const oneSecond = 1000;
@@ -15,8 +21,24 @@ export class Clock extends React.Component {
       this.setState({ date: new Date() });
     }, oneSecond);
   }
-  componentWillUnmount() {
+  componentDidUpdate(prevProps) {
+    if (this.props.isPrecise === prevProps.isPrecise) {
+      return;
+    }
     clearInterval(this.intervalID)
+    let delay;
+    if (this.props.isPrecise === true) {
+      delay = 100
+    } else {
+      delay = 1000
+    }
+    this.intervalID = setInterval(() => {
+      this.setState({ date: new Date() });
+    }, delay);
+  }
+  componentWillUnmount() {
+    clearInterval(this.intervalID);
   }
 }
+
 ReactDOM.render(<Clock />, document.getElementById('app'));
