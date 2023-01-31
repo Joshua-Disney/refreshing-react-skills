f3562aafaa4028c89cb9ca4278379c7db3f83d40
unknown <joshua.a.disney@gmail.com>
2023-01-30 23:37:10 -0700

    Complete function components lesson

--- Hooks/Function-Components/Friend.js
+++ Hooks/Function-Components/Friend.js
@@ -1,10 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-export class Friend extends React.Component {
-  render() {
+export const Friend = () => {
     return <img src="https://content.codecademy.com/courses/React/react_photo-octopus.jpg" />;
-  }
 };
 
 ReactDOM.render(
