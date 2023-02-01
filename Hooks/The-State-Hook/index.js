aa07f4d040ca913bd5def86cd0b04b794a6733e7
unknown <joshua.a.disney@gmail.com>
2023-01-31 23:47:06 -0700

    Ran out of spoons

--- Hooks/The-State-Hook/index.js
+++ Hooks/The-State-Hook/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./Container/AppClass";
-// import App from "./Container/AppFunction";
+// import App from "./Container/AppClass";
+import App from "./Container/AppFunction";
 
 ReactDOM.render(
   <App />,
