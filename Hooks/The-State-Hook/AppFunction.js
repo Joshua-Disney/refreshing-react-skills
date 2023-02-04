2417a4bfca4564f28cf5827df6d017a617722e47
unknown <joshua.a.disney@gmail.com>
2023-02-03 23:34:13 -0700

    Taking a break

--- Hooks/The-State-Hook/AppFunction.js
+++ Hooks/The-State-Hook/AppFunction.js
@@ -3,32 +3,36 @@ import NewTask from "../Presentational/NewTask";
 import TasksList from "../Presentational/TasksList";
 
 export default function AppFunction() {
-  const [newTask, setNewTask] = useState({});
-  const handleChange = ({ target }) => {
-    const { name, value } = target;
-    setNewTask((prev) => ({ ...prev, id: Date.now(), [name]: value }));
-  };
+  // hook your code up here ;)
+  const [newTast, setNewTask] = useState({})
+  const [allTasks, setAllTasks] = useState([])
 
-  const [allTasks, setAllTasks] = useState([]);
+  const handleChange = ({target}) => {
+    const {name, value} = target
+    setNewTask((prevTask) => ({
+      ...prevTask,
+      [name]: value,
+      id: Date.now()
+    }))
+  }
+
   const handleSubmit = (event) => {
-    event.preventDefault();
-    if (!newTask.title) return;
-    setAllTasks((prev) => [newTask, ...prev]);
-    setNewTask({});
-  };
-  const handleDelete = (taskIdToRemove) => {
-    setAllTasks((prev) => prev.filter((task) => task.id !== taskIdToRemove));
-  };
+    event.preventDefault()
+    if (!newTask.title) return
+    setAllTasks((prevAllTasks) => ([
+      newTask,
+      ...prevAllTasks
+    ]))
+  }
 
-  return (
-    <main>
-      <h1>Tasks</h1>
-      <NewTask
-        newTask={newTask}
-        handleChange={handleChange}
-        handleSubmit={handleSubmit}
-      />
-      <TasksList allTasks={allTasks} handleDelete={handleDelete} />
-    </main>
-  );
+  const handleDelete = (taskIdToRemove) => {
+    setAllTasks()
+  }
+
+  handleDelete(taskIdToRemove){
+    this.setState((prevState) => ({
+      ...prevState,
+      allTasks: prevState.allTasks.filter((task) => task.id !== taskIdToRemove)
+    }));
+  }
 }
